5e47e7743b1012e2fc2c37f2a8b468edce09b143
DanielOlivas <juandanielolivasmartinez@gmail.com>
2024-09-30 21:08:52 -0600

    Chat casi funcional XD

--- ChappMobileV2/Chat/ProyectosViewModel.cs
+++ ChappMobileV2/Chat/ProyectosViewModel.cs
@@ -2,17 +2,64 @@
 using System.ComponentModel;
 using System.Windows.Input;
 using Microsoft.Maui.Controls;
+using ChappMobileV2.Vistas;
+using System;
 
 namespace ChappMobileV2.ViewModels
 {
     public class ProyectosViewModel : INotifyPropertyChanged
     {
-        public ObservableCollection<Chat> Chats { get; set; }
+        private string _newMessage;
+        private int? _selectedChatId;
+
+        // Propiedad vinculada al Entry (para escribir nuevos mensajes)
+        public string NewMessage
+        {
+            get => _newMessage;
+            set
+            {
+                _newMessage = value;
+                OnPropertyChanged(nameof(NewMessage));
+            }
+        }
+
+        // Propiedad para almacenar el ID del chat seleccionado
+        public int? SelectedChatId
+        {
+            get => _selectedChatId;
+            set
+            {
+                _selectedChatId = value;
+                OnPropertyChanged(nameof(SelectedChatId));
+                LoadChatMessages(); // Cargar mensajes al seleccionar el chat
+            }
+        }
+
+        // Colección de mensajes del chat
         public ObservableCollection<Message> Messages { get; set; }
-        public ICommand NavigateToChatCommand { get; }
-        public ICommand GoBackToChatsCommand { get; }
+
+        // Colección de chats
+        public ObservableCollection<Chat> Chats { get; set; }
+
+        // Comando para enviar mensajes
         public ICommand SendMessageCommand { get; }
 
+        // Comando para navegar a un chat
+        public ICommand NavigateToChatCommand => new Command<int>(chatId =>
+        {
+            SelectedChatId = chatId;
+            IsChatDetailVisible = true; // Muestra la vista de detalles del chat
+            IsChatListVisible = false;  // Oculta la lista de chats
+        });
+
+        // Comando para volver a la lista de chats
+        public ICommand GoBackToChatsCommand => new Command(() =>
+        {
+            IsChatDetailVisible = false; // Oculta los detalles del chat
+            IsChatListVisible = true;     // Muestra la lista de chats
+        });
+
+        // Propiedades para controlar la visibilidad de las vistas
         private bool _isChatListVisible = true;
         public bool IsChatListVisible
         {
@@ -24,7 +71,7 @@ namespace ChappMobileV2.ViewModels
             }
         }
 
-        private bool _isChatDetailVisible = false;
+        private bool _isChatDetailVisible;
         public bool IsChatDetailVisible
         {
             get => _isChatDetailVisible;
@@ -35,55 +82,57 @@ namespace ChappMobileV2.ViewModels
             }
         }
 
+        // Constructor
         public ProyectosViewModel()
         {
-            // Inicializar los chats
-            Chats = new ObservableCollection<Chat>
+            // Inicialización de la colección de mensajes
+            Messages = new ObservableCollection<Message>
             {
-                new Chat { ChatId = 1, UserName = "Juan", LastMessage = "Hola!", ProfileImage = "juan.png" },
-                new Chat { ChatId = 2, UserName = "Maria", LastMessage = "Nos vemos luego", ProfileImage = "maria.png" }
+                new Message { UserName = "Juan", Content = "Hola, ¿cómo estás?", IsOwnMessage = false },
+                new Message { UserName = "Tú", Content = "Todo bien, ¿y tú?", IsOwnMessage = true }
             };
 
-            Messages = new ObservableCollection<Message>();
-
-            // Comandos
-            NavigateToChatCommand = new Command<int>(OnNavigateToChat);
-            GoBackToChatsCommand = new Command(GoBackToChats);
-            SendMessageCommand = new Command<string>(SendMessage);
-        }
-
-        private void OnNavigateToChat(int chatId)
-        {
-            // Cambiar la visibilidad
-            IsChatListVisible = false;
-            IsChatDetailVisible = true;
-
-            // Cargar mensajes simulados
-            Messages.Clear();
-            Messages.Add(new Message { UserName = "Juan", Content = "Hola, ¿cómo estás?" });
-            Messages.Add(new Message { UserName = "Tú", Content = "Todo bien, ¿y tú?" });
-        }
-
-        private void GoBackToChats()
-        {
-            // Cambiar la visibilidad
-            IsChatListVisible = true;
-            IsChatDetailVisible = false;
-        }
-
-        private void SendMessage(string messageContent)
-        {
-            if (!string.IsNullOrWhiteSpace(messageContent))
+            // Inicialización de la colección de chats
+            Chats = new ObservableCollection<Chat>
             {
-                Messages.Add(new Message { UserName = "Tú", Content = messageContent });
+                new Chat { ChatId = 1, UserName = "Juan", LastMessage = "Hola, ¿cómo estás?", ProfileImage = "profile1.png" },
+                new Chat { ChatId = 2, UserName = "María", LastMessage = "¿Te gustaría salir?", ProfileImage = "profile2.png" }
+            };
+
+            // Inicialización del comando de envío de mensajes
+            SendMessageCommand = new Command(SendMessage);
+        }
+
+        // Método para enviar mensajes
+        private void SendMessage()
+        {
+            if (!string.IsNullOrWhiteSpace(NewMessage))
+            {
+                // Añadir el mensaje a la lista de mensajes
+                Messages.Add(new Message { UserName = "Tú", Content = NewMessage, IsOwnMessage = true });
+
+                // Limpiar el Entry después de enviar el mensaje
+                NewMessage = string.Empty;
             }
         }
 
+        // Método para cargar los mensajes del chat seleccionado
+        private void LoadChatMessages()
+        {
+            if (SelectedChatId.HasValue)
+            {
+                // Lógica para cargar los mensajes del chat basado en SelectedChatId
+                // Por ejemplo, podrías filtrar una lista de mensajes previamente cargada
+            }
+        }
+
+        // Implementación del INotifyPropertyChanged para las actualizaciones de la UI
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged(string propertyName) =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 
+    // Modelo del chat
     public class Chat
     {
         public int ChatId { get; set; }
@@ -92,9 +141,11 @@ namespace ChappMobileV2.ViewModels
         public string ProfileImage { get; set; }
     }
 
+    // Modelo del mensaje
     public class Message
     {
         public string UserName { get; set; }
         public string Content { get; set; }
+        public bool IsOwnMessage { get; set; }
     }
 }
---
+++ ChappMobileV2/Converters/BoolToColorConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChappMobileV2.Converters
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Color.FromHex("#093A4F") : Colors.Gray; // Cambia los colores según tu preferencia
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
